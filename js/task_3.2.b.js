9ec6257f114300922f3957ad42a66e321fe28266
Mariia <mariia.neroznak@visma.com>
2022-04-01 16:01:40 +0300

    format

--- js/task_3.2.b.js
+++ js/task_3.2.b.js
@@ -1,14 +1,13 @@
 "use strict";
 
 const sum = (...args) => {
-    let sum = 0;
-  
-    for (let arg of args) {
-        if (isNaN(+arg)) return;
-        sum += +arg;
-    }
-    return sum;
+  let sum = 0;
+
+  for (let arg of args) {
+    if (isNaN(+arg)) return;
+    sum += +arg;
   }
-  
+  return sum;
+};
+
 module.exports = sum;
-  
